6072a08f158082cb1d5510330070f27fba6bdd42
Hussein Elzein <48137210+elzein22@users.noreply.github.com>
2019-05-15 09:15:09 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Components/Main';
+import './style/stylesheet.css';
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
